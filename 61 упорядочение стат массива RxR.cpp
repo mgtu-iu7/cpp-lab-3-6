e2cbf7b4bca6fd700224c6f152877988f6c94f79
МГТУ ИУ7 <del880@bk.ru>
2016-11-10 00:57:02 +0300

    Add files via upload

---
+++ 61 упорядочение стат массива RxR.cpp
@@ -0,0 +1,64 @@
+#include "stdafx.h"
+#include <math.h>
+#include <conio.h>
+#include <iostream>
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	setlocale(LC_ALL, "Russian");
+	const int R = 10;
+	int i(0), j(0), m(0), n(0), t(0);
+	int d(0),s(0);
+	int a[R][R] = { 0 };
+	srand((unsigned)time(NULL));
+	printf("\n\t Рандомная матрица:");
+	for (i = 0; i<R; i++)
+	{
+		printf("\n\t\t");
+		for (j = 0; j<R; j++)
+		{
+			a[i][j] = rand() % 10;
+			printf(" %d", a[i][j]);
+		}
+	}
+        do {
+			d = 0;
+		    for (j = 0; j < R; j++)
+		    {
+			  for (m = 0; m < R - 1; m++)
+			  {
+				 for (i = 0; i<R - 1 - m; i++)
+				 {
+					if (a[i][j]>a[i + 1][j])
+					{
+						t = a[i][j];
+						a[i][j] = a[i + 1][j];
+						a[i + 1][j] = t;
+						d = 1;
+					}
+				 }
+			  }
+			if ((a[R - 1][j]>a[0][j + 1]) && (j <= R - 2))
+		    {
+			 t = a[R - 1][j];
+			 a[R - 1][j] = a[0][j + 1];
+		     a[0][j + 1] = t;
+			 d = 1;
+			  }
+		   }
+		} while (d>0);
+		 printf("\n\t Упорядоченная матрица:\n\t");
+		 for (i = 0; i<R; i++)
+		 {
+			printf("\n\t\t");
+			for (j = 0; j<R; j++)
+			{
+				printf(" %d", a[i][j]);
+			}
+		 }
+	_getch();
+	return 0;
+}
